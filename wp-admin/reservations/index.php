1c14e1e6d1a53e3f1c390b3f04717ad7b6341223
Moritz Bürger <moritz.buerger@prodyna.com>
2016-09-25 21:51:24 +0200

    possible to add reservations

--- wp-admin/reservations/index.php
+++ wp-admin/reservations/index.php
@@ -4,8 +4,62 @@ require_once( dirname( __FILE__ ) . '/../admin.php' );
 
 get_header();
 
+global $wpdb;
+
+$tableName = 'reservations';
 $pageName = 'Reservierungen';
+$format = 'd.m.Y';
+
+
+function hasCollision($arrivalDate, $departureDate) {
+    global $wpdb, $tableName;
+    $query = "SELECT COUNT(*)
+              FROM $tableName
+              WHERE
+                (end >= '$arrivalDate' AND end <= '$departureDate')
+                OR
+                (start <= '$arrivalDate' AND start >= '$departureDate')";
+    $reservations = $wpdb->get_var($query);
+    return intval($reservations) > 0;
+}
+
+function saveToDataBase($arrivalTime, $departureTime, $name) {
+    global $wpdb;
+    if(!($arrivalTime && $departureTime && $name)) {
+        return -1;
+    }
+    $arrivalDate = date("Y-m-d", $arrivalTime);
+    $departureDate = date("Y-m-d", $departureTime);
+    if(hasCollision($arrivalDate, $departureDate)) {
+        return -2;
+    }
+    $result = $wpdb->insert('reservations', array(
+        'name' => $name,
+        'start' => $arrivalDate,
+        'end' => $departureDate
+    ));
+    if($result === 1) {
+        return $wpdb->insert_id;
+    }
+    return -3;
+}
+
+function formInput($paramKey, $paramValue, $valid, $fieldName, $type = 'text') {
+    $inputClass = $valid ? '' : ' invalid';
+    $message = $valid ? '' : "<p class='form-error-message'>$fieldName ist ungültig.</p>";
+    echo "<div class='form-group'>
+            <label for='arrival_day'>$fieldName:</label>
+              <input class='form-control$inputClass'
+                     type='$type'
+                     id='$paramKey'
+                     name='$paramKey'
+                     value='$paramValue'
+                     required>
+                     $message
+          </div>";
+}
 ?>
+
 
 <div class="container">
     <div class="row">
@@ -21,29 +75,91 @@ $pageName = 'Reservierungen';
                         <div class="entry-meta"></div>
                     </header>
 
+                    <?php
+
+                    $arrivalKey = 'arrival_day';
+                    $arrivalValue = htmlspecialchars($_POST[$arrivalKey]);
+                    $arrivalTime = strtotime($arrivalValue);
+                    $departureKey = 'departure_day';
+                    $departureValue = htmlspecialchars($_POST[$departureKey]);
+                    $departureTime = strtotime($departureValue);
+                    $nameKey = 'name';
+                    $nameValue = htmlspecialchars($_POST[$nameKey]);
+                    $nameValid = $nameValue != null && $nameValue != '';
+
+                    $wrongOrderMessage = '';
+                    $message = '';
+
+                    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+                        if($arrivalTime && $departureTime) {
+
+                            if($arrivalTime < $departureTime) {
+                                $success = saveToDataBase($arrivalTime, $departureTime, $nameValue);
+
+                                if($success > 0) {
+                                    $arrivalDatePretty = date($format, $arrivalTime);
+                                    $departureDatePretty = date($format, $departureTime);
+                                    $message = "<p>Die Reservierung ($arrivalDatePretty bis $departureDatePretty) wurde erfolgreich eingetragen.</p>";
+                                    $arrivalValue = '';
+                                    $departureValue = '';
+                                    $nameValue = '';
+                                } else {
+                                    $errorCode = -$success;
+                                    $message = "<p>Beim Eintragen der Reservierung ist ein Fehler aufgetreten. (Code: 52$errorCode)";
+                                }
+                            } else {
+                                $wrongOrderMessage =
+                                    '<p class="form-error-message">Anreisetag muss <strong>vor</strong> dem Abreisetag liegen</p>';
+                            }
+                        }
+
+                    } else {
+                        $arrivalTime = true;
+                        $departureTime = true;
+                        $nameValid = true;
+                    }
+
+                    ?>
 
                     <div class="entry-content">
+
                         <div class="c-grubeWidget c-reservations">
                             <a id="reservations"></a>
                             <h3>Reservierung hinzufügen</h3>
-                            <form method="get" action="#reservations">
-                                <div class="form-group">
-                                    <label for="name">Name:</label>
-                                    <input class="form-control" type="text" id="name" name="name" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="arrival_day">Anreisetag:</label>
-                                    <input class="form-control" type="date" id="arrival_day" name="arrival_day" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="departure_day">Abreisetag:</label>
-                                    <input class="form-control" type="date" id="departure_day" name="departure_day" required>
-                                </div>
+                            <form method="post" action="#reservations">
+                                <?php
+                                    formInput($nameKey, $nameValue, $nameValid, 'Name');
+                                    formInput($arrivalKey, $arrivalValue, $arrivalTime, 'Anreisetag', 'date');
+                                    formInput($departureKey, $departureValue, $departureTime, 'Abreisetag', 'date');
+
+                                    echo $wrongOrderMessage;
+                                ?>
+
                                 <button type="submit" class="btn btn-primary">Speichern</button>
                             </form>
                         </div>
+                        <p></p>
+                        <?php
+                        if($message) {
+                            echo "<div class='alert alert-success text-left'>$message</div>";
+                        }
+                        ?>
+                        <ul class="list-group">
+                            <?php
+                                $now = date("Y-M-d");
+                                $query = "SELECT * FROM reservations WHERE '$now' <= end ORDER BY start";
+                                $reservations = $wpdb->get_results($query, OBJECT);
 
-                        <?php echo htmlspecialchars($_GET['arrival_day']); ?>
+                                foreach($reservations as $reservation) {
+                                    $start = date($format, strtotime($reservation->start));
+                                    $end = date($format, strtotime($reservation->end));
+                                    $name = $reservation->name;
+
+                                    echo "<li class='list-group-item'><div>$name</div><div>$start - $end</div></li>";
+                                }
+                            ?>
+                        </ul>
 
                     </div>
 
